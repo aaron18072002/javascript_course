91b4657b6178299d417c9a0d0028a2909cd6b860
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-21 17:41:16 +0700

    updated

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+
+const number = 100;
+console.log(number);
+
+let otherNumber = 200;
+console.log(otherNumber);
+otherNumber = 500;
+console.log(otherNumber);
+otherNumber = "welcome to my javascript course";
+console.log(otherNumber);
+
+// hoisting
+console.log(anotherNumber);
+const anotherNumber = 120;
