ba7dda5ea9d17c02594eba7760013a34bc8dfebf
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-12-21 19:45:45 +0700

    updated

---
+++ part3.js
@@ -0,0 +1,34 @@
+"4.5"; // 4.5 parseFloat
+"4"; // 4 parseInt
+
+// Number(value)
+console.log("4.5 --> " + Number("4.5"));
+console.log("4 --> " + Number("4"));
+console.log("string --> " + Number("abcdef"));
+
+// NaN: not a number
+console.log("NaN --> " + Number(NaN)); //NaN
+console.log("null --> " + Number(null)); // 0
+console.log("true --> " + Number(true)); // 1
+console.log("false --> " + Number(false)); // 0
+console.log('"" --> ' + Number("")); // 0
+
+// String(value)
+console.log("=================STRING==================");
+console.log("4.5 --> ", String(4.5));
+console.log("4 --> ", String(4));
+console.log("undefined --> ", String(undefined));
+console.log("NaN --> ", String(NaN));
+console.log("false --> ", String(false));
+
+// Boolean(value) --> true hoặc false
+console.log("=================BOOLEAN==================");
+console.log("0 --> " + Boolean(0));
+console.log('"" --> ' + Boolean(""));
+console.log("null --> " + Boolean(null));
+console.log("NaN --> " + Boolean(NaN));
+console.log("undefined --> " + Boolean(undefined));
+console.log("false --> " + Boolean(false));
+console.log("Dev0 Fpt --> " + Boolean("Dev0 Fpt"));
+
+// Type coercion - Ép kiểu
