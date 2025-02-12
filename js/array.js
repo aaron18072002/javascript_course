317ef83d1692ca0ed5bba63fe71d1eea0da41d30
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-02-12 16:54:12 +0700

    updated

--- js/array.js
+++ js/array.js
@@ -4,3 +4,38 @@ const a = "a";
 const b = "b";
 const c = "c";
 const d = "d";
+
+// 2 cách tạo mảng
+
+// array literal
+// const students1 = [];
+// array constructor
+// const students2 = new Array();
+// ví dụ 1 mảng
+// Mảng phức tạp [0,false,undefined,null,"evondev",["evondev",false,1200, []]]
+// Mảng đơn giản [0,false,undefined,null,"evondev"]
+// []: empty array -> mảng rỗng
+let students1 = ["evondev", "tuan", "nam", "thanh", "trung"];
+// index: vị trí của phần tử(giá trị) trong mảng và bắt đầu từ 0
+// length: độ dài của mảng, nó đếm từ 1
+// truy xuất trong mảng -> students1[index]
+console.log(students1[0]);
+// phần tử cuối cùng trong mảng ở vị trí length - 1
+console.log(students1[students1.length - 1]);
+// students1.length = 0;
+console.log(students1);
+// []: mảng rỗng
+
+// phương thức hay dùng của mảng trong js
+console.log("arr.length --> " + students1.length);
+console.log("reversed arr --> " + students1.reverse());
+
+// join -> nối các phần tử trong mảng với 1 chuỗi --> trả về 1 array mới
+console.log("join --> " + students1.join("123"));
+
+students1 = ["evondev", "tuan", "nam", "thanh", "trung"];
+// includes --> kiểm tra mảng đó có chứa phần tử nào đó ko
+console.log("test include evondev --> " + students1.includes("evondev"));
+console.log("test include a1 --> " + students1.includes("a1"));
+
+// indexOf --> trả về vị trí của 1 phần tử trong mảng
