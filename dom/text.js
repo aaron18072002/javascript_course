dcf5b76785ea9d53b4cc7e54f2abfca3cce06526
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2025-02-13 16:36:04 +0700

    updated

--- dom/text.js
+++ dom/text.js
@@ -1,3 +1,15 @@
-// 1. textContent
+// 1. textContent --> ko lấy được nội dung đã bị ẩn đi
+
 const spinner = document.querySelector("#spinner");
+// thay đổi textContent
+// spinner.textContent = "Hello new content";
 console.log(spinner.textContent);
+
+// 2. innerText
+console.log(spinner.innerText);
+
+// spinner.textContent = "<div class=``></div>"; --> in ra string <div>...</div> luôn
+
+// 3. innerHTML --> lấy ra nội dung của selector bao gồm cả HTML
+console.log(spinner.innerHTML);
+spinner.innerHTML = `<div class = "demo">hello html</div>`;
